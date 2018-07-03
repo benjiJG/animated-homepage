c6f94d93191e84fd4a2902d664a1f9539bd83f77
Ben <benjii.goodman@hotmail.com>
2018-07-03 16:39:10 +0100

    added mug and improved existing animations

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,5 +1,4 @@
-var leftLeg = $('#leftLeg');
-    rightLeg = $('#rightLeg');
-    top = $('#top');
-
-TweenMax.from(leftLeg, 0.5, {bottom: '-=100px', ease:Fade.easeOut});
+TweenMax.from('#top', 1, {bottom: '-=300px', rotation: 180, ease: SlowMo.easeOut, delay: 0.5});
+TweenMax.from('#leftLeg', 0.8, {bottom: '-=300px', ease: Bounce.easeOut, delay: 0});
+TweenMax.from('#rightLeg', 0.8, {bottom: '-=300px', ease: Bounce.easeOut, delay: 0});
+TweenMax.from('#mug', 0.5, {scaleY: 0, transformOrigin: "left", delay: 1.3});
