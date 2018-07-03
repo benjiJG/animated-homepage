69f84b2ffa94763abed4132b9b9e9252b49589a1
Ben <benjii.goodman@hotmail.com>
2018-07-03 16:45:52 +0100

    fixed scale transform on mug to start from bottom

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,4 +1,4 @@
 TweenMax.from('#top', 1, {bottom: '-=300px', rotation: 180, ease: SlowMo.easeOut, delay: 0.5});
 TweenMax.from('#leftLeg', 0.8, {bottom: '-=300px', ease: Bounce.easeOut, delay: 0});
 TweenMax.from('#rightLeg', 0.8, {bottom: '-=300px', ease: Bounce.easeOut, delay: 0});
-TweenMax.from('#mug', 0.5, {scaleY: 0, transformOrigin: "left", delay: 1.3});
+TweenMax.from('#mug', 0.5, {scaleY: 0, transformOrigin: "50% bottom", ease: Sine.easeIn, delay: 1.3});
