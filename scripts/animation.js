f024ca7341ef3469c61618e2fcf103ee56208e66
Ben <benjii.goodman@hotmail.com>
2018-07-03 17:09:58 +0100

    added pc div and changed all containers to fixed positioning

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,4 +1,9 @@
-TweenMax.from('#top', 1, {bottom: '-=300px', rotation: 180, ease: SlowMo.easeOut, delay: 0.5});
-TweenMax.from('#leftLeg', 0.8, {bottom: '-=300px', ease: Bounce.easeOut, delay: 0});
-TweenMax.from('#rightLeg', 0.8, {bottom: '-=300px', ease: Bounce.easeOut, delay: 0});
-TweenMax.from('#mug', 0.5, {scaleY: 0, transformOrigin: "50% bottom", ease: Sine.easeIn, delay: 1.3});
+$('document').ready(function(){
+  TweenMax.from('#top', 1, {bottom: '-=300px', rotation: 360, ease: SlowMo.easeOut, delay: 0.5});
+  TweenMax.from('#leftLeg', 0.8, {bottom: '-=300px', ease: Bounce.easeOut, delay: 0});
+  TweenMax.from('#rightLeg', 0.8, {bottom: '-=300px', ease: Bounce.easeOut, delay: 0});
+
+  TweenMax.from('#mug', 0.5, {scaleY: 0, transformOrigin: "50% bottom", ease: Sine.easeIn, delay: 1.3});
+
+  TweenMax.from('#pcStand', 0.5, {scaleY: 0, transformOrigin: "50% bottom", ease: Sine.easeIn, delay: 2});
+});  
