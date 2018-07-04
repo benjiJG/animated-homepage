94f65d921307c014be92ba795c2c6a7825fdb03c
Ben <benjii.goodman@hotmail.com>
2018-07-04 10:25:28 +0100

    added monitor and screen, adjusted some animations

--- scripts/animation.js
+++ scripts/animation.js
@@ -5,5 +5,9 @@ $('document').ready(function(){
 
   TweenMax.from('#mug', 0.5, {scaleY: 0, transformOrigin: "50% bottom", ease: Sine.easeIn, delay: 1.3});
 
-  TweenMax.from('#pcStand', 0.5, {scaleY: 0, transformOrigin: "50% bottom", ease: Sine.easeIn, delay: 2});
+  TweenMax.from('#pcStand', 0.5, {scaleY: 0, transformOrigin: "50% bottom", ease: Bounce.easeOut, delay: 2});
+  TweenMax.from('#pcMonitor', 0.5, {scaleY: 0, transformOrigin: "50% bottom", ease: Bounce.easeOut, delay: 2.5});
+  TweenMax.from('#pcScreen', 0.5, {scaleY: 0, scaleX: 0, opacity: 0.8, transformOrigin: "50% 50%", ease: Elastic.easeOut, delay: 3});
+
+  
 });  
